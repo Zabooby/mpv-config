bf75ce85899108c3010e8ed8a2da4b787bd6205b
Zabooby <78969986+Zabooby@users.noreply.github.com>
2022-12-25 15:37:13 +0000

    Update adasharpA.glsl

--- portable_config/shaders/adasharpA.glsl
+++ portable_config/shaders/adasharpA.glsl
@@ -31,10 +31,10 @@
 
 //--------------------------------------- Settings ------------------------------------------------
 
-#define curve_height    1.0                  // Main control of sharpening strength [>0]
+#define curve_height    0.6                  // Main control of sharpening strength [>0]
                                              // 0.3 <-> 2.0 is a reasonable range of values
 
-#define overshoot_ctrl  false                // Allow for higher overshoot if the current edge pixel
+#define overshoot_ctrl  true                 // Allow for higher overshoot if the current edge pixel
                                              // is surrounded by similar edge pixels
 
 // Defined values under this row are "optimal" DO NOT CHANGE IF YOU DO NOT KNOW WHAT YOU ARE DOING!
