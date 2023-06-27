6d5db41ca7cfeb3d660358bb7852fee3d8071340
Zabooby <78969986+Zabooby@users.noreply.github.com>
2023-06-27 21:06:11 +0100

    Update nlmeans_hqx.glsl

--- portable_config/shaders/nlmeans_hqx.glsl
+++ portable_config/shaders/nlmeans_hqx.glsl
@@ -116,15 +116,18 @@
  * 	  - 2: sharpen only
  * ASF: Higher numbers make a sharper image
  * ASA: Anti-ringing, higher numbers increase strength
+ * ASP: Power, lower numbers increase sharpening on lower frequency detail
  */
 #ifdef LUMA_raw
 #define AS 0
-#define ASF 1.0
+#define ASF 0.1625
 #define ASA 5.0
+#define ASP 0.5
 #else
 #define AS 0
-#define ASF 1.0
+#define ASF 0.1625
 #define ASA 5.0
+#define ASP 0.5
 #endif
 
 /* Starting weight
@@ -1102,6 +1105,7 @@ vec4 hook()
 #endif
 #if AS
 	  val usm = result - sum_s/total_weight_s;  
+	  usm = exp(log(abs(usm))*ASP) * sign(usm);   // avoiding pow() since it's buggy on nvidia
 	  usm *= gaussian(abs((AS_base + usm - 0.5) / 1.5) * ASA);  
 	  usm *= ASF;  
 	  result = AS_base + usm;  
@@ -1187,15 +1191,18 @@ return _INJ_RF_LUMA_texOff(0);
  * 	 - 2: sharpen only
  * ASF: Higher numbers make a sharper image
  * ASA: Anti-ringing, higher numbers increase strength
+ * ASP: Power, lower numbers increase sharpening on lower frequency detail
  */
 #ifdef LUMA_raw
 #define AS 0
-#define ASF 1.0
+#define ASF 0.1625
 #define ASA 5.0
+#define ASP 0.5
 #else
 #define AS 0
-#define ASF 1.0
+#define ASF 0.1625
 #define ASA 5.0
+#define ASP 0.5
 #endif
 
 /* Starting weight
@@ -2173,6 +2180,7 @@ vec4 hook()
 #endif
 #if AS
 	 val usm = result - sum_s/total_weight_s; 
+	 usm = exp(log(abs(usm))*ASP) * sign(usm);  // avoiding pow() since it's buggy on nvidia
 	 usm *= gaussian(abs((AS_base + usm - 0.5) / 1.5) * ASA); 
 	 usm *= ASF; 
 	 result = AS_base + usm; 
@@ -2257,15 +2265,18 @@ vec4 hook()
  * 	- 2: sharpen only
  * ASF: Higher numbers make a sharper image
  * ASA: Anti-ringing, higher numbers increase strength
+ * ASP: Power, lower numbers increase sharpening on lower frequency detail
  */
 #ifdef LUMA_raw
 #define AS 0
-#define ASF 1.0
+#define ASF 0.1625
 #define ASA 5.0
+#define ASP 0.5
 #else
 #define AS 0
-#define ASF 1.0
+#define ASF 0.1625
 #define ASA 5.0
+#define ASP 0.5
 #endif
 
 /* Starting weight
@@ -3243,6 +3254,7 @@ vec4 hook()
 #endif
 #if AS
 	val usm = result - sum_s/total_weight_s;
+	usm = exp(log(abs(usm))*ASP) * sign(usm); // avoiding pow() since it's buggy on nvidia
 	usm *= gaussian(abs((AS_base + usm - 0.5) / 1.5) * ASA);
 	usm *= ASF;
 	result = AS_base + usm;
